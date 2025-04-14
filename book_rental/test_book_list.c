7c5cc2e2c2a7eb5a37064e8962b337a939019b29
sharpscar <sharpscar@gmail.com>
2025-04-15 06:59:15 +0900

    응?

---
+++ book_rental/test_book_list.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "../cJSON/cJSON.h"
+
+
+int main()
+{
+    // no, title, writer, publisher, pub_year, count, isbn, add_sign, kdc, kcd_genre,frequency_of_book_rental
+}
